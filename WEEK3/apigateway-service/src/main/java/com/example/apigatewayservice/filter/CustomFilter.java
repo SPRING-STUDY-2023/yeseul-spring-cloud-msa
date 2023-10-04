4c68f872fbf6bc1e72e31921e445e236c79a8beb
yeseul106 <20191037@sungshin.ac.kr>
2023-10-04 12:59:48 +0900

    [WEEK3] Section2 - CustomFilter까지

---
+++ WEEK3/apigateway-service/src/main/java/com/example/apigatewayservice/filter/CustomFilter.java
@@ -0,0 +1,39 @@
+package com.example.apigatewayservice.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
+    @Override
+    public GatewayFilter apply(Config config) {
+        // Custom Pre filter
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("Custom Pre filter: request id = {}", request.getId());
+
+            // Custom Post filter
+            return chain.filter(exchange)
+                    .then(Mono.fromRunnable( //Mono라는 객체는 WebFlux 라고 해서 스프링5에 추가되어있는 기능 -> 비동기 방식의 서버를 지원할때 단일값 전달할 때 사용
+                            () -> log.info("Custom Post filter: respose code = {}", response.getStatusCode())));
+        };
+    }
+
+    public CustomFilter() {
+        super(Config.class);
+    }
+
+    public static class Config {
+        // Put the configuration properties
+    }
+
+}
---
+++ WEEK3/second-service/src/main/java/com/example/secondservice/controller/SecondServiceController.java
@@ -0,0 +1,2 @@
+package com.example.secondservice.controller;public class SecondServiceController {
+}
